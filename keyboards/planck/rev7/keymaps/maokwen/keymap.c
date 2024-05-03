b926ddfb5b24c81e83a678fa5be2ad46da724d4d
maokwen <18361388+maokwen@users.noreply.github.com>
2024-05-04 06:46:59 +0800

    * auto shift

--- keyboards/planck/rev7/keymaps/maokwen/keymap.c
+++ keyboards/planck/rev7/keymaps/maokwen/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     KC_TAB , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_DQUO,
     KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_ENT ,
-    KC_LCTL, KC_LGUI, KC_LALT, FUNC   , LOWER  , KC_SPC          , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT 
+    KC_LCTL, KC_LGUI, KC_LALT, FUNC   , LOWER  , KC_SPC          , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
 ),
 
 /* Lower: left hand - numbers, operators, punctuators
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------|------+
  * |      |      |CV(LT)|CV(DN)|CV(RT)|      |      | Left | Down | Right|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------|------+
- * |      |      |      |      |      |      |      | Home |      |  End |      |      |
+ * |AShift|      |      |      |      |      |      | Home |      |  End |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------|------+
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_mit(
     _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP  , KC_PGDN, _______, KC_DEL ,
     _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, KC_END , _______, _______,
+    AS_TOGG, _______, _______, _______, _______, _______, _______, KC_HOME, _______, KC_END , _______, _______,
     _______, _______, _______, _______, _______, _______         , _______, _______, _______, _______, _______
 ),
 
@@ -162,17 +162,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Layer
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    if (layer_state_cmp(state, _LOWER) && 
+    if (layer_state_cmp(state, _LOWER) &&
         (layer_state_cmp(state, _RAISE) || layer_state_cmp(state, _FUNC))) {
         reset_oneshot_layer();
     }
-    
+
     return update_tri_layer_state(state, _FUNC, _RAISE, _ADJUST);
 }
 
 #ifdef AUDIO_ENABLE
-float shot_timeout_song[][2] = SONG(E__NOTE(_E7), E__NOTE(_E6));
-float layer1_shot_song[][2] = SONG(E__NOTE(_E6), E__NOTE(_E7));
+float shot_timeout_song[][2] = SONG(Q__NOTE(_E4));
+float layer1_shot_song[][2] = SONG(Q__NOTE(_E2));
 
 void oneshot_layer_changed_user(uint8_t layer) {
     switch (layer)
